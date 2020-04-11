715a145455224d5426df7a99e14181d67a99da60
Milan Balabanovic <miki.balabanovic111@gmail.com>
2020-04-11 20:59:12 +0200

    Working ELO system

--- main.py
+++ main.py
@@ -1,5 +1,52 @@
 import playerStats
+import game
 
-p1 = playerStats.Player("user1", 340)
+players = []
+nop = input("Number of players: ")
 
-print(playerStats.Player.checkRank(p1))
+for i in range(int(nop)):
+	user = input("Username of player {0}: ".format(i+1))
+	players.append(playerStats.Player(user, 1000))
+
+
+def startGame():
+	selected = []
+	expected = [] # Format: p1, p2
+
+	print("Users and their ELO points")
+	for p in players:
+		print("{0} ........ {1}".format(p.user, p.elo))
+
+	for i in range(2):
+		s = input("Insert username of the Player {0}: ".format(i+1))
+
+		for player in players:
+			if s == player.user:
+				selected.append(player)
+
+	print("---- Simulated outcome ----------------------")
+
+	# Calculate outcome and expected result
+	outcome = game.calculateOutcome()
+	expected = game.calculateWP(selected[0], selected[1])
+
+
+	selected[0].updateElo(abs(outcome), expected[0])
+	selected[1].updateElo(abs(1-outcome), expected[1])
+
+	# Print calculated data
+
+	print("Chances to win:")
+	print("{0} : {1}".format(selected[0].user, expected[0]))
+	print("{0} : {1}".format(selected[1].user, expected[1]))
+	print("\n")
+	if outcome == 1:
+		print("{0} wins!".format(selected[0].user))
+	else:
+		print("{0} wins!".format(selected[1].user))
+
+	print("\n")
+
+
+while(1):
+	startGame()
--- playerStats.py
+++ playerStats.py
@@ -11,19 +11,26 @@ class Player:
 		self.user = user
 		self.elo = points
 
+
+	#Check for rank based on elo points
 	def checkRank(self):
 		elo = self.elo
 
-		if elo < 300:
-			if elo < 200:
-				if elo < 100:
+		if elo < 3000:
+			if elo < 2000:
+				if elo < 1000:
 					return "Rank E"
 				else:
 					return "Rank D"
 			else:
 				return "Rank C"
 		else:
-			if elo > 400:
+			if elo > 4000:
 				return "Rank A"
 			else:
 				return "Rank B"
+
+
+	#Update elo points
+	def updateElo(self, score, expected):
+		self.elo = self.elo + int(32 * (score - expected))
